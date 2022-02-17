b22373006baeab1cda763e096419315c5a03a8fd
NicolasMacedoo <nicolas.conceicao01@gmail.com>
2022-02-17 17:35:48 -0300

    Create variables with image path values

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let doorImage1 = document.getElementById('door1');
 let doorImage2 = document.getElementById('door2');
 let doorImage3 = document.getElementById('door3');
 let startButton = document.getElementById('start');
+const botDoorPath = './resources/images/robot.svg';
+const beachDoorPath = './resources/images/beach.svg';
+const spaceDoorPath = './resources/images/space.svg';
+const closedDoorPath = './resources/images/closed_door.svg';
