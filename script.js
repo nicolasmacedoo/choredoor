712bd155f4f927867e24aa628cde5e73b3e4ce64
NicolasMacedoo <nicolas.conceicao01@gmail.com>
2022-02-17 17:31:27 -0300

    Create variables with values from html

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+let doorImage1 = document.getElementById('door1');
+let doorImage2 = document.getElementById('door2');
+let doorImage3 = document.getElementById('door3');
+let startButton = document.getElementById('start');
